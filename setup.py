729994076de59525251812d136b3d872b6fe7968
Joshua Burt <joshburt@Joshuas-MBP.lan>
2023-01-27 09:29:46 -0800

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="anaconda.mlflow.tracking.sdk",
-    version="0.10.0",
+    version="0.10.1",
     package_dir={"": "src"},
     packages=setuptools.find_namespace_packages(where="src"),
     author="Joshua C. Burt",
